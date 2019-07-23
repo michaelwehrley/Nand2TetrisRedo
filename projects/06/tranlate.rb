5fdaad91ad34ddc37282e7683e9e283200a0cd29
Michael Wehrley <michaelwehrley@gmail.com>
2019-07-22 22:51:11 -0500

    finishing up comps

--- projects/06/tranlate.rb
+++ projects/06/tranlate.rb
@@ -1,3 +1,4 @@
+require "byebug"
 class Assembler
   attr_reader :file, :hack_file
 
@@ -81,6 +82,9 @@ class CInstruction < Instruction
   end
 
   def comp_cs
+    return unless cs?
+
+    COMP_TABLE[line.match(/=(.*)/)[1].strip!.to_sym]
   end
 
   def comp
@@ -91,10 +95,16 @@ class CInstruction < Instruction
     line.match(/=.*(M)/).nil?
   end
 
-  def cs
+  def cs?
+    !line.match(/=(.*)/).nil? # TODO for comments..
   end
 
   def dest
+    return "000" unless dest?
+    "#{destination_a}#{destination_d}#{destination_m}"
+  end
+
+  def dest?
   end
 
   def jump
@@ -118,8 +128,8 @@ SYMBOL_TABLE = {
   R13: 13,
   R14: 14,
   R15: 15,
-  SCREEN: 16384,
-  KBD: 24576,
+  SCREEN: 16_384,
+  KBD: 24_576,
   SP: 0,
   LCL: 1,
   ARG: 2,
@@ -127,9 +137,36 @@ SYMBOL_TABLE = {
   THAT: 4
 }
 
-# COMP_TABLE = {
-#   10
-# }
+COMP_TABLE = {
+  "0": "101010",
+  "1": "111111",
+  "-1": "111010",
+  "D": "001100",
+  "A": "110000",
+  "M": "110000",
+  "!D": "001101",
+  "!A": "110001",
+  "!M": "110001",
+  "-D": "001111",
+  "-A": "110011",
+  "-M": "110011",
+  "D+1": "011111",
+  "A+1": "110111",
+  "M+1": "110111",
+  "D-1": "001110",
+  "A-1": "110010",
+  "M-1": "110010",
+  "D+A": "000010",
+  "D+M": "000010",
+  "D-A": "010011",
+  "D-M": "010011",
+  "A-D": "000111",
+  "M-D": "000111",
+  "D&A": "000000",
+  "D&M": "000000",
+  "D|A": "010101",
+  "D|M": "010101"
+}.freeze
 
 # 1. Ignore white space and comments
 # 1. Instructions
