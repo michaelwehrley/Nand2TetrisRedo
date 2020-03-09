fcfc01a8ecf75a9ada7c3e0abf0bc5cc4ea0cbac
Michael Wehrley <michaelwehrley@gmail.com>
2020-03-08 22:06:22 -0500

    poping

--- projects/07/VMTranslator.rb
+++ projects/07/VMTranslator.rb
@@ -46,7 +46,18 @@ class VMTranslate
     File.open(File.join(File.dirname(__FILE__), "#{file}.vm")).each do |line|
       line = line.gsub(/\/{2}.*/, "").strip
       next if white_space_or_comment?(line)
-      
+      # ... remember
+      # @SP get this address or const
+      # D=M set it to D
+      # A=M set the address register to the value of the M register which got set on @
+      # M=D Set the memory of reg A to D
+
+      # SIMPLY set RAM[3012] with 40
+      # @40
+      # D=A
+      # @3012
+      # M=D
+
       append_comment("#{line}")
       if line == "add"
         byebug
@@ -57,17 +68,31 @@ class VMTranslate
       elsif parse(line)[1] == "push" && %w(local argument that this temp).include?(parse(line)[2])
         append("@#{TRANSLATIONS[parse(line)[2].to_sym]}\nD=M")
         append("@#{parse(line)[3]}")
-        append("D=D+A") # go to the correct relative memory address
+        append("A=D+A\nD=M") # go to the correct relative memory address and set pushed value on to SP and increment
         append("@SP\nA=M\nM=D")
         increment_stack_pointer
       elsif parse(line)[1] == "pop" && %w(local argument that this temp).include?(parse(line)[2])
         append("@#{TRANSLATIONS[parse(line)[2].to_sym]}\nD=M")
         append("@#{parse(line)[3]}")
-        append("D=D+A") # go to the correct relative memory address
+        append("A=D+A\nD=M") # get current value on SP and pop to new segment
         decrement_stack_pointer
         append("@SP\nA=M\nM=D")
+        # // pop that 2
+        # @THAT
+        # D=M
+        # @2
+        # // go to new address and get value to pop...
+        # A=D+A
+        # D=M
+        # // decrement
+        # @SP
+        # M=M-1
+        # // set new value
+        # @SP
+        # A=M
+        # M=D
       else
-        byebug
+        # byebug
       end
     end
   end
