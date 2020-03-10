5375b674c80983c6efcaa249338de4533adcdaea
Michael Wehrley <michaelwehrley@gmail.com>
2020-03-09 23:12:04 -0500

    update pop

--- projects/07/MemoryAccess/BasicTest/BasicTest.asm
+++ projects/07/MemoryAccess/BasicTest/BasicTest.asm
@@ -1,30 +1,253 @@
-@333
+// push constant 10
+@10
 D=A
-@255
-M=D
-
-// pop local 0
-
-@SP
-M=M-1
-
 @SP
 A=M
-
-D=M
-@value
 M=D
-
+@SP
+M=M+1
+// pop local 0
+@SP
+A=M
+D=M
+@stackValue
+M=D
 @LCL
 D=M
 @0
 D=D+A
-@targetLCL
+@targetLocation
 M=D
-
-@value
+@stackValue
 D=M
-@targetLCL
+@targetLocation
+M=D
+@SP
+M=M-1
+// push constant 21
+@21
+D=A
+@SP
 A=M
 M=D
-
+@SP
+M=M+1
+// push constant 22
+@22
+D=A
+@SP
+A=M
+M=D
+@SP
+M=M+1
+// pop argument 2
+@SP
+A=M
+D=M
+@stackValue
+M=D
+@ARG
+D=M
+@2
+D=D+A
+@targetLocation
+M=D
+@stackValue
+D=M
+@targetLocation
+M=D
+@SP
+M=M-1
+// pop argument 1
+@SP
+A=M
+D=M
+@stackValue
+M=D
+@ARG
+D=M
+@1
+D=D+A
+@targetLocation
+M=D
+@stackValue
+D=M
+@targetLocation
+M=D
+@SP
+M=M-1
+// push constant 36
+@36
+D=A
+@SP
+A=M
+M=D
+@SP
+M=M+1
+// pop this 6
+@SP
+A=M
+D=M
+@stackValue
+M=D
+@THIS
+D=M
+@6
+D=D+A
+@targetLocation
+M=D
+@stackValue
+D=M
+@targetLocation
+M=D
+@SP
+M=M-1
+// push constant 42
+@42
+D=A
+@SP
+A=M
+M=D
+@SP
+M=M+1
+// push constant 45
+@45
+D=A
+@SP
+A=M
+M=D
+@SP
+M=M+1
+// pop that 5
+@SP
+A=M
+D=M
+@stackValue
+M=D
+@THAT
+D=M
+@5
+D=D+A
+@targetLocation
+M=D
+@stackValue
+D=M
+@targetLocation
+M=D
+@SP
+M=M-1
+// pop that 2
+@SP
+A=M
+D=M
+@stackValue
+M=D
+@THAT
+D=M
+@2
+D=D+A
+@targetLocation
+M=D
+@stackValue
+D=M
+@targetLocation
+M=D
+@SP
+M=M-1
+// push constant 510
+@510
+D=A
+@SP
+A=M
+M=D
+@SP
+M=M+1
+// pop temp 6
+@SP
+A=M
+D=M
+@stackValue
+M=D
+@5
+D=A
+@6
+D=D+A
+@targetLocation
+M=D
+@stackValue
+D=M
+@targetLocation
+M=D
+@SP
+M=M-1
+// push local 0
+@LCL
+D=M
+@0
+A=D+A
+D=M
+@SP
+A=M
+M=D
+@SP
+M=M+1
+// push that 5
+@THAT
+D=M
+@5
+A=D+A
+D=M
+@SP
+A=M
+M=D
+@SP
+M=M+1
+// add
+// push argument 1
+@ARG
+D=M
+@1
+A=D+A
+D=M
+@SP
+A=M
+M=D
+@SP
+M=M+1
+// sub
+// push this 6
+@THIS
+D=M
+@6
+A=D+A
+D=M
+@SP
+A=M
+M=D
+@SP
+M=M+1
+// push this 6
+@THIS
+D=M
+@6
+A=D+A
+D=M
+@SP
+A=M
+M=D
+@SP
+M=M+1
+// add
+// sub
+// push temp 6
+@5
+D=A
+@6
+A=D+A
+D=M
+@SP
+A=M
+M=D
+@SP
+M=M+1
+// add
--- projects/07/VMTranslator.rb
+++ projects/07/VMTranslator.rb
@@ -1,13 +1,92 @@
 require "byebug"
 
 class VMTranslate
-  attr_reader :assembly_file, :file, :line_count
+  attr_reader :assembly_file, :file
+
+  TRANSLATIONS = {
+    local: "LCL",
+    argument: "ARG",
+    this: "THIS",
+    that: "THAT",
+    temp: nil
+  }.freeze
 
   def initialize(file)
     @file = file    
     @assembly_file = File.open(File.join(File.dirname(__FILE__), "#{file}.asm"), "w")
-    @line_count = 0
-    # @ram_count = 16
+  end
+
+  def write
+    File.open(File.join(File.dirname(__FILE__), "#{file}.vm")).each do |line|
+      line = line.gsub(/\/{2}.*/, "").strip
+      next if white_space_or_comment?(line)
+
+      append_comment(line)
+      action = parse(line)[1]
+      segment = parse(line)[2]
+      value = parse(line)[3]
+      register = register_name(segment)
+
+      if action == "add"
+        byebug
+      elsif action == "push" && segment == "constant"
+        append("@#{value}")
+        push_constant
+      elsif action == "push" && segment?(segment)
+        push(register, value, segment == "temp")
+      elsif action == "pop" && segment?(segment)
+        pop(register, value, segment == "temp")
+      else
+        byebug
+      end
+    end
+  end
+
+  private
+
+  def pop(register, target, is_temp)
+    # get value that is current on top of stack
+    append("@SP")
+    append("A=M")
+    append("D=M")
+    append("@stackValue")
+    append("M=D") # storing value to be popped in temp
+
+    # get relative address
+    append(is_temp ? "@5" : register)
+    append(is_temp ? "D=A" : "D=M")
+    append("@#{target}")
+    append("D=D+A")
+    append("@targetLocation")
+    append("M=D") # store relative address in target address
+
+    # put stack value in target location
+    append("@stackValue")
+    append("D=M")
+    append("@targetLocation")
+    append("M=D")
+
+    decrement_stack_pointer
+  end
+
+  def push_constant
+    append("D=A")
+    append("@SP")
+    append("A=M")
+    append("M=D")
+    increment_stack_pointer
+  end
+
+  def push(register, target, is_temp)
+    append(is_temp ? "@5" : register)
+    append(is_temp ? "D=A" : "D=M")
+    append("@#{target}")
+    append("A=D+A")
+    append("D=M")
+    append("@SP")
+    append("A=M")
+    append("M=D")
+    increment_stack_pointer
   end
 
   def append(content)
@@ -18,83 +97,32 @@ class VMTranslate
     append("// #{content}")
   end
 
-  def increment_stack_pointer
-    append("@SP\nM=M+1")
-  end
-
   def decrement_stack_pointer
     append("@SP\nM=M-1")
   end
 
+  def increment_stack_pointer
+    append("@SP\nM=M+1")
+  end
+
   def parse(line)
-    /(^\w+)\s(\w+)\s(\d+$)/.match(line)
+    /(^\w+)\s(\w+)\s(\d+$)/.match(line) || /(^\w+$)/.match(line)
   end
 
   def white_space_or_comment?(line)
     line.length == 0 || line.match(/^\/\//)
   end
 
-  TRANSLATIONS = {
-    local: "LCL",
-    argument: "ARG",
-    this: "THIS",
-    that: "THAT",
-    temp: 5
-  }.freeze
+  def register_name(segment)
+    return unless segment
 
-  def write
-    File.open(File.join(File.dirname(__FILE__), "#{file}.vm")).each do |line|
-      line = line.gsub(/\/{2}.*/, "").strip
-      next if white_space_or_comment?(line)
-      # ... remember
-      # @SP get this address or const
-      # D=M set it to D
-      # A=M set the address register to the value of the M register which got set on @
-      # M=D Set the memory of reg A to D
+    "@#{TRANSLATIONS[segment.to_sym]}"
+  end
 
-      # SIMPLY set RAM[3012] with 40
-      # @40
-      # D=A
-      # @3012
-      # M=D
+  def segment?(segment)
+    return false unless segment
 
-      append_comment("#{line}")
-      if line == "add"
-        byebug
-      elsif parse(line)[1] == "push" && parse(line)[2] == "constant"
-        append("@#{parse(line)[3]}")
-        append("D=A\n@SP\nA=M\nM=D")
-        increment_stack_pointer
-      elsif parse(line)[1] == "push" && %w(local argument that this temp).include?(parse(line)[2])
-        append("@#{TRANSLATIONS[parse(line)[2].to_sym]}\nD=M")
-        append("@#{parse(line)[3]}")
-        append("A=D+A\nD=M") # go to the correct relative memory address and set pushed value on to SP and increment
-        append("@SP\nA=M\nM=D")
-        increment_stack_pointer
-      elsif parse(line)[1] == "pop" && %w(local argument that this temp).include?(parse(line)[2])
-        append("@#{TRANSLATIONS[parse(line)[2].to_sym]}\nD=M")
-        append("@#{parse(line)[3]}")
-        append("A=D+A\nD=M") # get current value on SP and pop to new segment
-        decrement_stack_pointer
-        append("@SP\nA=M\nM=D")
-        # // pop that 2
-        # @THAT
-        # D=M
-        # @2
-        # // go to new address and get value to pop...
-        # A=D+A
-        # D=M
-        # // decrement
-        # @SP
-        # M=M-1
-        # // set new value
-        # @SP
-        # A=M
-        # M=D
-      else
-        # byebug
-      end
-    end
+    TRANSLATIONS.keys.include?(segment.to_sym)
   end
 end
 
